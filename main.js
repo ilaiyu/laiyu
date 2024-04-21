2ba6172feaafd71843e8af09acea268b9e7636a2
lai_chao@outlook.com <lai_chao@outlook.com>
2024-04-21 14:07:54 +0800

    fix

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const giscusTheme = () =>
     localStorage.getItem('DARK_LIGHT_THEME') === 'dark' ? 'noborder_dark' : 'light';
 
 window.$docsify = {
+    hideCode: {
+        scroll: false, // 启用滚动
+        height: 300 // 折叠高度
+    },
+    subMaxLevel: 4,
     name: 'advanced-java',
     repo: 'doocs/advanced-java',
-    maxLevel: 3,
+    maxLevel: 4,
     auto2top: true,
-    coverpage: true,
     coverpage: 'docs/extra-page/cover.md',
     loadSidebar: 'summary.md',
     alias: {
@@ -31,11 +35,26 @@ window.$docsify = {
     search: {
         maxAge: 1800000,
         paths: [
-            '/docs/high-concurrency/',
-            '/docs/distributed-system/',
-            '/docs/high-availability/',
-            '/docs/micro-services/',
             '/docs/big-data/',
+            '/docs/distributed-system/',
+            '/docs/Dubbo/',
+            '/docs/extra-page/',
+            '/docs/high-concurrency/',
+            '/docs/high-availability/',
+            '/docs/JDK/',
+            '/docs/LeaningExperience/',
+            '/docs/micro-services/',
+            '/docs/Mybatis/',
+            '/docs/nacos/',
+            '/docs/Netty/',
+            '/docs/Redis/',
+            '/docs/rocketmq/',
+            '/docs/Sentinel/',
+            '/docs/Spring/',
+            '/docs/Springboot/',
+            '/docs/SpringbootBatch/',
+            '/docs/SpringCloud/',
+            '/docs/SpringSecurity/',
             '/docs/Tomcat/'
         ],
         depth: 3,
@@ -55,86 +74,5 @@ window.$docsify = {
             highlightColor: '#e96900',
         },
     },
-    plugins: [
-        function (hook, vm) {
-            hook.beforeEach(function (content) {
-                const { file, path } = vm.route;
-                const en = file.indexOf('README_EN') > -1;
-                if (/githubusercontent\.com/.test(file)) {
-                    url = file
-                        .replace(
-                            'raw.githubusercontent.com',
-                            'github.com',
-                        )
-                        .replace(/\/main/, '/blob/main');
-                } else {
-                    url = `https://github.com/doocs/advanced-java/blob/main/${file}`;
-                }
-
-                const github = `[GitHub](${url})`;
-                const gitee = `[Gitee](${url.replace('github', 'gitee')})`;
-
-                const editHtml = en
-                    ? `:memo: Edit on ${github} / ${gitee}\n`
-                    : `:memo: 在 ${github} / ${gitee} 编辑\n`;
-
-                if (path === '/') {
-                    return editHtml + content;
-                }
-                const subscription = `---
-## 公众号
-
-[Doocs](https://github.com/doocs) 技术社区旗下唯一公众号「**Doocs**」​，欢迎扫码关注，**专注分享技术领域相关知识及业内最新资讯**。当然，也可以加我个人微信（备注：GitHub），拉你进技术交流群。
-
-关注「**Doocs**」公众号，回复 **PDF**，即可获取本项目离线 PDF 文档，学习更加方便！
-
-<table>
-  <tr>
-    <td align="center" style="width: 260px;">
-      <img src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/images/qrcode-for-doocs.png" style="width: 400px;"><br>
-    </td>
-    <td align="center" style="width: 260px;">
-      <img src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/images/qrcode-for-yanglbme.png" style="width: 400px;"><br>
-    </td>
-  </tr>
-</table>`;
-                return editHtml + content + `\n` + subscription;
-            });
-
-            hook.afterEach(function (html) {
-                const currentYear = new Date().getFullYear();
-                const footer = `<footer><span>Copyright © 2018-${currentYear} <a href="https://github.com/doocs" target="_blank">Doocs</a>. All Rights Reserved.</footer>`;
-                return html + footer;
-            });
-            hook.doneEach(() => {
-                const giscusScript = document.createElement('script');
-                giscusScript.type = 'text/javascript';
-                giscusScript.async = true;
-                giscusScript.setAttribute('src', 'https://giscus.app/client.js');
-                giscusScript.setAttribute('data-repo', 'doocs/advanced-java');
-                giscusScript.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnkxNTE4MzQwNjI=');
-                giscusScript.setAttribute('data-mapping', 'number');
-                giscusScript.setAttribute('data-reactions-enabled', '1');
-                giscusScript.setAttribute('data-strict', '1');
-                giscusScript.setAttribute('data-emit-metadata', '0');
-                giscusScript.setAttribute('data-input-position', 'top');
-                giscusScript.setAttribute('crossorigin', 'anonymous');
-                giscusScript.setAttribute('data-term', '9');
-                giscusScript.setAttribute('data-lang', 'zh-CN');
-                giscusScript.setAttribute('data-theme', giscusTheme());
-
-                document
-                    .getElementById('main')
-                    .insertBefore(giscusScript, document.getElementById('main').lastChild);
-
-                document.getElementById('docsify-darklight-theme').addEventListener('click', () => {
-                    const frame = document.querySelector('.giscus-frame');
-                    frame.contentWindow.postMessage(
-                        { giscus: { setConfig: { theme: giscusTheme() } } },
-                        'https://giscus.app',
-                    );
-                });
-            })
-        },
-    ],
+    plugins: [],
 };
